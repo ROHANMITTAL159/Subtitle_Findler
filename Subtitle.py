f0795341f79c60e97d544b9ce87036b9fa390a81
rohanmittal159 <rohanmittal159@gmail.com>
2019-03-27 20:32:54 +0530

    Updated code

--- Subtitle.py
+++ Subtitle.py
@@ -7,27 +7,31 @@ Created on Mon Mar 25 23:44:33 2019
 # Subtitle Findler
 import requests
 from bs4 import BeautifulSoup
-count = 0
-usearch = input("Movie Name? : ")
-search_url = "https://www.yifysubtitles.com/search?q="+usearch.replace(" ","+")
-base_url = "https://www.yifysubtitles.com"
-print(search_url)
-resp = requests.get(search_url)
-soup = BeautifulSoup(resp.content, 'lxml')
-for link in soup.find_all("div",{"class": "media-body"}):       
-    imdb = link.find('a')['href']                           #to find out movie url     
-    movie_url = base_url+imdb                                   
-    print("Movie URL : {}".format(movie_url))               #print the url to verify 
- 
-    next_page = requests.get(movie_url)                         
-    soup2 = BeautifulSoup(next_page.content,'lxml')
-    for links in soup2.find_all("tr",{"class": "high-rating"}): 
-        for flags in links.find("td", {"class": "flag-cell"}):  
-            if flags.text == "English":                         
-                for dlink in links.find_all("td",{"class": "download-cell"}):   
-                    half_link = dlink.find('a')['href']    #to find subtitle     
-                    subtitle = base_url + half_link
-                    if len(half_link) > 0:                  #print the link to subtitle file
-                        print("Movie subtitle-link:  {}".format(subtitle))
-                    else:
-                        print("No Subtitle found!!! Thank You for visiting")
+from os import listdir
+filenames = listdir('D:\Video Tutorial\Movies')
+u = [f for f in filenames if f.endswith(".mp4")]
+for usearch in u:
+    index_of_dot = usearch.index('.')
+    usearch = usearch[:index_of_dot]
+    search_url = "https://www.yifysubtitles.com/search?q="+usearch.replace(" ","+")
+    base_url = "https://www.yifysubtitles.com"
+    print(search_url)
+    resp = requests.get(search_url)
+    soup = BeautifulSoup(resp.content, 'lxml')
+    for link in soup.find_all("div",{"class": "media-body"}):       
+        imdb = link.find('a')['href']                           #to find out movie url     
+        movie_url = base_url+imdb                                   
+        print("Movie URL : {}".format(movie_url))               #print the url to verify 
+     
+        next_page = requests.get(movie_url)                         
+        soup2 = BeautifulSoup(next_page.content,'lxml')
+        for links in soup2.find_all("tr",{"class": "high-rating"}): 
+            for flags in links.find("td", {"class": "flag-cell"}):  
+                if flags.text == "English":                         
+                    for dlink in links.find_all("td",{"class": "download-cell"}):   
+                        half_link = dlink.find('a')['href']    #to find subtitle     
+                        subtitle = base_url + half_link
+                        if len(half_link) > 0:                  #print the link to subtitle file
+                            print("Movie subtitle-link:  {}".format(subtitle))
+                        else:
+                            print("No Subtitle found!!! Thank You for visiting")
